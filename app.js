4b894e98a86eba5a71a383bc08b1fe4d55a71835
vesper <vesperjahid@gmail.com>
2019-10-28 02:00:25 +0600

    More function are added with previous

--- app.js
+++ app.js
@@ -21,4 +21,43 @@ output = name.toLowerCase();
 // position of keyword using index
 
 output= intro.indexOf("javascript");
+
+//substring  (works from left to right)
+
+output = intro.substring(4); /// output: ome to javascript tutorial
+output = intro.substring(0,10); /// output: welcome to
+
+/// slice (works from right to left)
+
+output = intro.slice(-6); /// output: tutorial
+
+/// slice (works from left to right)
+output = intro.slice(0,10); /// output: tutorial   
+
+
+/// split 
+
+output = intro.split(" ");
+
+/* output:   
+(4) ["welcome", "to", "javascript", "tutorial"]
+0: "welcome"
+1: "to"
+2: "javascript"
+3: "tutorial"
+*/
+
+
+/// Replace 
+
+output = intro.replace("javascript","js");
+
+
+/// include (check string are avaiable or not, return true or false)
+
+output = intro.includes("javascript");
+
+/// Repeat 
+
+output = name.repeat("6");
 console.log(output);
