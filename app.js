235f89fcf195f36ab44c344135e33f92c16ca4db
vesper <vesperjahid@gmail.com>
2019-10-28 01:14:10 +0600

    data type of variables

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-let name = prompt("What is your name?");
-document.getElementById('name').innerHTML='welcome ' + name;
-   
+let name = "jahid";
+let number = 123;
+let bool = true;
+let language = ['HTML5,CSS,JS'];
+let per = {
+    name: "jahid",
+    id:05907172
+}
+
+//date
+
+let date = new Date();
+console.log(typeof name);
+console.log(typeof number);
+//boolean
+console.log(typeof bool);
+//array
+console.log(typeof language);
+//object
+console.log(typeof per);
+//date
+console.log(typeof date);
