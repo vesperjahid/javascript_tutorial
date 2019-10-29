fe3ebdd7f116b1f7f3da4228f768be82248ede0a
vesper <vesperjahid@gmail.com>
2019-10-30 02:30:45 +0600

    More useful array function

--- app.js
+++ app.js
@@ -1,37 +1,97 @@
-/// Template design old methord
+////   array create
 
-const product1 = 'Pizza',
-      price1 = 80,
-      product2 = 'Burger',
-      price2 = 60;
+/// index array
 
+const number = [1,2,3,4,5];
 
-      let html;
-
-      html = '<ul>'+
-                '<li>Item :' + product1 + '</li>' +
-                '<li>Price :' + price1 + '</li>' +
-                '<li>Item :' + product2 + '</li>' +
-                '<li>Price :' + price2 + '</li>' +
-                '<li>Total = ' + (price1+price2) + '</li>' +
-             '</ul>';
-
-
-/////// Template  string or template literales
-
-html = `
-    <ul>
-        <li>Item: ${product1}</li>
-        <li>Price: ${price1}</li>
-        <li>Item: ${product2}</li>
-        <li>Price: ${price2}</li>
-        <li>Total: ${price1+price2}</li>
-    </ul>
-`;
+console.log(number);  /// indexing
 
 /*
-    The new way is more clean & user friendly.
+Output: (5) [1, 2, 3, 4, 5]
+0: 1
+1: 2
+2: 3
+3: 4
+4: 5
+length: 5
 */
 
-let app = document.querySelector('#templateapp');
-app.innerHTML = html;
+// another methord to create array
+
+const months = new Array('jan','feb','mar','april','may','june');
+
+console.log(months);
+
+////  length of array
+
+console.log(months.length) // output : 6
+
+
+////// More useful function for array
+
+// find data in array
+
+console.log(months.indexOf('mar')); /// output: 2
+
+// add new data in array at last possition
+
+months.push('july');
+console.log(months); 
+/* output:
+["jan", "feb", "mar", "april", "may", "june", "july"]
+0: "jan"
+1: "feb"
+2: "mar"
+3: "april"
+4: "may"
+5: "june"
+6: "july"
+*/
+
+// add new data in array at fast possition
+
+months.unshift('Months:');
+console.log(months);
+/* output:
+ 0: "Months:"
+1: "jan"
+2: "feb"
+3: "mar"
+4: "april"
+5: "may"
+6: "june"
+7: "july"
+*/
+
+// add new data in array at middle or any possition
+
+months.splice(2, 0, "aug","Sep");
+console.log(months);
+/* output:
+0: "Months:"
+1: "jan"
+2: "aug"
+3: "Sep"
+4: "feb"
+5: "mar"
+6: "april"
+7: "may"
+8: "june"
+9: "july"
+*/
+
+/// remove data from array last
+
+months.pop();
+console.log(months);
+
+/// remove data from array fast
+
+months.shift();
+console.log(months);
+
+/// remove data from array any position
+
+months.splice(3,2); /// 3 where to cut, 2 how much cut
+console.log(months);
+
