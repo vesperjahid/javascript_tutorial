7a08163f7692e8a568b129efc742e8c4f02b507c
vesper <vesperjahid@gmail.com>
2019-10-30 03:42:21 +0600

    const array & object

--- app.js
+++ app.js
@@ -1,47 +1,12 @@
-////   array object
+//// constant in array
 
-let Employee = {
-    name: 'Jahidul islam',
-    age: 27,
-    job: 'Web developer',
-    email: 'vesper@muslim.com',
-    favoriteMusic: ['bangla','english','hindi'],
-    living:{
-        City:'Dhaka',
-        Country:"Bangladesh"
-    },
-    bornYear: function(){
-        return new Date().getFullYear() - this.age;
-    },
-    getJob: function(){
-        return this.job;
-    }
- 
-}
+const num = [1,2,3];
+// const change is changeable bt indevisual
+num[0]= 4;
 
-console.log(Employee.name);
-console.log(Employee.age);
-console.log(Employee.job);
-console.log(Employee.email);
-console.log(Employee.favoriteMusic);
-console.log(Employee.favoriteMusic[2]);
-console.log(Employee.living);
-console.log(Employee.living.City);
-console.log(Employee.bornYear());
-console.log(Employee.getJob());
+/// also insert data
+
+num.push(7);
 
 
-
-//// another way to create array
-
-let car = [
-    {model:"Toyota", speed:'300cc'},
-    {model:"Xcorola", speed:'250cc'},
-    {model:"oddi", speed: '380cc'}
-];
-
-for(let i=0;i<car.length;i++){
-    console.log(car[i]);
-    console.log(car[i].model);
-    console.log(car[i].speed);
-}
+console.log(num)
