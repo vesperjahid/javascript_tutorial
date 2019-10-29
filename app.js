3fc854bb3ccdcba2623499493387f61ae2a28346
vesper <vesperjahid@gmail.com>
2019-10-30 02:48:13 +0600

    Array sort or order methord

--- app.js
+++ app.js
@@ -1,97 +1,40 @@
-////   array create
+/// array sort
 
-/// index array
+/// reverse array item
 
-const number = [1,2,3,4,5];
-
-console.log(number);  /// indexing
-
-/*
-Output: (5) [1, 2, 3, 4, 5]
-0: 1
-1: 2
-2: 3
-3: 4
-4: 5
-length: 5
-*/
-
-// another methord to create array
 
 const months = new Array('jan','feb','mar','april','may','june');
 
-console.log(months);
+months.reverse();
 
-////  length of array
+// concatenate 2 aray
 
-console.log(months.length) // output : 6
+let num = [1,2,3],
+    num2 = [4,5,6,7];
+
+console.log(num.concat(num2)); /// output: 1,2,3,4,5,6,7
+
+////// order array item
+
+console.log(months.sort()); /// sort by assending order
+
+///// sort by desending order. this is tricky part. if we reverse the assendenting item . it will desending order
+
+console.log(months.sort().reverse());
 
 
-////// More useful function for array
+///// number order by lower to grather
 
-// find data in array
+let arrayNumber= [1,12,112,2,23,34,56,64,21,456,787,98];
 
-console.log(months.indexOf('mar')); /// output: 2
+console.log(arrayNumber.sort(function(number1,number2){
+    return number1 - number2;
+}));
 
-// add new data in array at last possition
 
-months.push('july');
-console.log(months); 
-/* output:
-["jan", "feb", "mar", "april", "may", "june", "july"]
-0: "jan"
-1: "feb"
-2: "mar"
-3: "april"
-4: "may"
-5: "june"
-6: "july"
-*/
+///// number order by grather to lower
 
-// add new data in array at fast possition
-
-months.unshift('Months:');
-console.log(months);
-/* output:
- 0: "Months:"
-1: "jan"
-2: "feb"
-3: "mar"
-4: "april"
-5: "may"
-6: "june"
-7: "july"
-*/
-
-// add new data in array at middle or any possition
-
-months.splice(2, 0, "aug","Sep");
-console.log(months);
-/* output:
-0: "Months:"
-1: "jan"
-2: "aug"
-3: "Sep"
-4: "feb"
-5: "mar"
-6: "april"
-7: "may"
-8: "june"
-9: "july"
-*/
-
-/// remove data from array last
-
-months.pop();
-console.log(months);
-
-/// remove data from array fast
-
-months.shift();
-console.log(months);
-
-/// remove data from array any position
-
-months.splice(3,2); /// 3 where to cut, 2 how much cut
-console.log(months);
+console.log(arrayNumber.sort(function(number1,number2){
+    return number2 - number1;
+}));
 
