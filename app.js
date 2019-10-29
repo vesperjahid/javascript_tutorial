267909d92852257fc5b71f6fec902c72c5185f5b
vesper <vesperjahid@gmail.com>
2019-10-30 04:25:27 +0600

    useses of date methord

--- app.js
+++ app.js
@@ -1,57 +1,22 @@
-/// function  of js
+//date of js
+
+// create object for date
+
+const today = new Date();
+let output;
+
+output = new Date('auguest 17 1992');  ///  month day year
+
+output = today.getMonth();
+output = today.getDay();  ////0 = sunday, 1 = monday so on
+output = today.getFullYear();
+output = today.getHours();
+output = today.getMinutes();
+output = today.getSeconds();
+today.setMonth(5);
+output = today.getMonth();
+today.setFullYear(2000);
+output = today.getFullYear();
 
 
-///  basic function expression
-
-// addition
-
-function addition(num1='0',num2='0'){
-    return Number(num1) + Number(num2);
-}
-
-console.log(addition(12,10));
-console.log(addition(1,15));
-console.log(addition(2,1));
-
-
-/// modulus 
-
-function checkNumber(Number=0){
-    return Number%2
-}
-if(checkNumber(8)==0){
-    console.log("enen");
-}else{
-    console.log("Odd");
-}
-
-
-/// imvoked function
-
-(function(name){
-    console.log("hello "+name);
-})("vesper")
-
-
-///// function with methord
-
-const musicplayer = {
-    play: function(id){
-        console.log(`Music play with id ${id}`);
-    },
-    pause: function(){
-        console.log('pause');
-    }
-}
-
-musicplayer.play(20);
-
-musicplayer.pause();
-
-//// can be create methord out side the function
-
-musicplayer.remove = function(id){
-    console.log(`${id} is removed from playlist`);
-}
-
-musicplayer.remove(20);
+console.log(output);
